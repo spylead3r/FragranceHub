7edeb8bed52470bc89588d42d1acf7424e13804a
spyleader@students.softuni.bg <spyleader@students.softuni.bg>
2024-04-14 00:24:12 +0300

    new

---
+++ FragranceHub.Data/Migrations/20240413201600_CustomRegister.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FragranceHub.Data.Migrations
+{
+    public partial class CustomRegister : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Fragrances",
+                keyColumn: "Id",
+                keyValue: new Guid("25a8792e-1f1d-4128-af62-23bd68e50481"));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "PublishedOnDate",
+                table: "Fragrances",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2024, 4, 13, 20, 15, 59, 750, DateTimeKind.Utc).AddTicks(1314),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2023, 8, 13, 1, 46, 32, 67, DateTimeKind.Utc).AddTicks(6387));
+
+            migrationBuilder.InsertData(
+                table: "Fragrances",
+                columns: new[] { "Id", "CategoryId", "Description", "ImageUrl", "IsApproved", "Name", "Price", "UserId", "WishlistId" },
+                values: new object[] { new Guid("d8e50449-adc8-4460-9488-6884e2091333"), 1, "Creed Aventus is a timeless men's fragrance, exuding confidence and power. With notes of pineapple, blackcurrant, and musk, it evokes a bold and unforgettable aura.", "https://i.makeup.bg/r/rv/rvuuikzs9nz6.jpg", true, "Creed Aventus", 620.00m, null, null });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Fragrances",
+                keyColumn: "Id",
+                keyValue: new Guid("d8e50449-adc8-4460-9488-6884e2091333"));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "PublishedOnDate",
+                table: "Fragrances",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2023, 8, 13, 1, 46, 32, 67, DateTimeKind.Utc).AddTicks(6387),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2024, 4, 13, 20, 15, 59, 750, DateTimeKind.Utc).AddTicks(1314));
+
+            migrationBuilder.InsertData(
+                table: "Fragrances",
+                columns: new[] { "Id", "CategoryId", "Description", "ImageUrl", "IsActive", "IsApproved", "Name", "Price", "PublishedOnDate", "UserId", "WishlistId" },
+                values: new object[] { new Guid("25a8792e-1f1d-4128-af62-23bd68e50481"), 1, "Creed Aventus is a timeless men's fragrance, exuding confidence and power. With notes of pineapple, blackcurrant, and musk, it evokes a bold and unforgettable aura.", "https://i.makeup.bg/r/rv/rvuuikzs9nz6.jpg", false, true, "Creed Aventus", 620.00m, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null, null });
+        }
+    }
+}
